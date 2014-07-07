5b4330b9dc49b7e816426fc6d1035f4bebc26df7
Sam <sam@laptop.com>
2014-07-06 22:03:14 -0400

    Add first F# problem.

---
+++ FSharp/001.fsx
@@ -0,0 +1,14 @@
+(* 
+   Problem 1
+   =========
+
+   If we list all the natural numbers below 10 that are multiples of 3 or 5,
+   we get 3, 5, 6 and 9. The sum of these multiples is 23.
+   
+   Find the sum of all the multiples of 3 or 5 below 1000. *)
+ 
+
+let answer = [ 1 .. 999 ] |> List.filter (fun i -> i % 3 = 0 || i % 5 = 0) |> List.sum
+printfn "%i" answer
+
+System.Console.ReadKey() |> ignore
